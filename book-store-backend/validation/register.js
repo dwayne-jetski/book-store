5229cd0670a068e9e2a3e9522f08a7f77e8c89c8
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2021-01-28 18:59:15 -0600

    added validation/login.js and validation/register.js to backend. Finished login validation. added first nameand last name to UserSchema in models/user.js

---
+++ book-store-backend/validation/register.js
@@ -0,0 +1,53 @@
+const { date } = require('joi');
+const { default: validator } = require('validator');
+const Validator = require('validator');
+const isEmpty = ('is-empty');
+
+module.exports = function validateRegisterInput(data) {
+    let errors = {};
+
+    //convert empty fields to an empty string so we can use validator functions
+    data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+    data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
+    data.email = !isEmpty(data.email) ? data.email: "";
+    data.password = !isEmpty(data.password) ? data.password: "";
+    data.password2 = !isEmpty(data.password2) ? data.password: "";
+
+    //name checks
+    if(Validator.isEmpty(data.firstName)) {
+        errors.Firstname = "First Name field is required...";
+    }
+
+    if(Validator.isEmpty(data.lastName)) {
+        errors.lastName = "Last Name field is required...";
+    }
+
+    //email checks
+    if(Validator.isEmpty(data.email)) {
+        errors.email = "Email field is required...";
+    } else if (!Validator.isEmail(data.email)) {
+        errors.email = "Email is invalid...";
+    }
+
+    //password checks 
+    if (Validator.isEmpty(data.password)) {
+        errors.password = "Password field is required...";
+    }
+
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = "Confirm password field is required...";
+    }
+
+    if (!Validator.isLength(data.password, {min: 8, max: 32})) {
+        errors.password2 = "Password must be at least 6 characters..."
+    }
+
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = "Passwords must match";
+    }
+
+    return {
+        errors,
+        isValid: isEmpty (errors)
+    };
+};
